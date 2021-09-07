fdb6b1f0407ae916e7213d7d324b6ad257b26c1b
yourcarrot0214 <stylenbs@gmail.com>
2021-09-07 17:17:02 +0900

    디렉토리 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
 import React from "react";
-import "./App.css";
-// import Greetings from "./Greetings";
-// import Counter from "./Counter";
-// import MyForm from "./MyForm";
-import ReducerSample from "./ReducerSample";
-import { SampleProvider } from "./SampleContext";
 
 function App() {
-  // const handleButton = (name: string) => console.log(name);
-  // const onSubmit = (form: { name: string; description: string }) => {
-  //   console.log(form);
-  // };
-  return (
-    <div className='App'>
-      {/* <Greetings name='carrot' onClick={handleButton} /> */}
-      {/* <Counter /> */}
-      {/* <MyForm onSubmit={onSubmit} /> */}
-      <SampleProvider>
-        <ReducerSample />
-      </SampleProvider>
-    </div>
-  );
+  return <div className='App'>Hello Typescript</div>;
 }
 
 export default App;
